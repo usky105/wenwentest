5e1ee85c01793b9987eae14723859b6292dd1914
usky <usky105@163.com>
2018-05-27 21:39:44 +0800

    Initial commit

---
+++ src/main/java/com/wenwen/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.wenwen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/body/{x}", method = RequestMethod.GET)
+    public bodytest getBody(@PathVariable("x") String x){
+        System.out.println("URI Part 1 : " + x);
+        bodytest bt = new bodytest();
+        bt.a = x;
+        bt.b = "1234";
+        return bt;
+    }
+
+    public class  bodytest
+    {
+        public  String a;
+        public String b;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! nice </h2>
+</body>
+</html>
